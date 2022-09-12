592936aa6baa46e91012da86436e2cf5b1a13839
daniela-idara <purpleelephant1987@gmail.com>
2022-09-12 01:40:12 -0500

    fetching internet data

--- learning-python-2896241(jul2022)/Ch5 - Internet Data/inetdata_start.py
+++ learning-python-2896241(jul2022)/Ch5 - Internet Data/inetdata_start.py
@@ -2,9 +2,12 @@
 # Example file for retrieving data from the internet
 # LinkedIn Learning Python course by Joe Marini
 #
+import urllib.request
 
 def main():
-    pass # this is a placeholder, do-nothing statement
-
+    weburl = urllib.request.urlopen("http://www.dropbox.com")
+    print("result code:", weburl.getcode()) ## return code know if it the request was reachable
+    data = weburl.read()    ## Adding the read the code for the webpage is viewed. 
+    print(data)             ## the print result displays the code
 if __name__ == "__main__":
     main()
